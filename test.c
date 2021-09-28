c47f56adbe3369bf554f117dfe576ca64027b2fb
Bpatel <bishal.patel@itron.com>
2021-09-28 09:36:28 -0500

    Changed the bir_year to birth_year just to make is look common and rearranged spacing.

--- test.c
+++ test.c
@@ -1,1 +1,18 @@
 //structure with fields with name. dob,address, get the values.
+//#include <stdio.h>
+
+int main(void)
+{
+    char firstname[], lastname[];
+
+    int birth_year;
+
+    printf( "Input your firstname: " );
+    scanf ( "%s", firstname );
+    printf( "Input your lastname: " );
+    scanf ( "%s", lastname );
+    printf( "Input your year of birth: " );
+    scanf ( "%d", &birth_year );
+    printf( "%s %s %d\n", firstname, lastname, birth_year );
+
+    return 0;
